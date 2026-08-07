9e1926fb12aaefc8855881397965c7b4f43d8725
Kenny Glowner <SisyphusAeolides@pm.me>
2026-08-07 06:47:53 -0500

    Build DNS interface lookup

--- build.rs
+++ build.rs
@@ -40,6 +40,7 @@ fn compile_c(cc: &OsString, source: &str, output: &Path) {
 
 fn main() {
     println!("cargo:rerun-if-changed=ffi/native.c");
+    println!("cargo:rerun-if-changed=ffi/interface.c");
     println!("cargo:rerun-if-changed=ffi/netlink.c");
     println!("cargo:rerun-if-changed=ffi/networkd.c");
     println!("cargo:rerun-if-changed=ffi/native.h");
@@ -57,13 +58,15 @@ fn main() {
     let cc = env::var_os("CC").unwrap_or_else(|| OsString::from("cc"));
     let ar = env::var_os("AR").unwrap_or_else(|| OsString::from("ar"));
     let native_obj = object(&out_dir, "resolved_native");
+    let interface_obj = object(&out_dir, "resolved_interface");
     let netlink_obj = object(&out_dir, "resolved_netlink");
     let networkd_obj = object(&out_dir, "resolved_networkd");
     compile_c(&cc, "ffi/native.c", &native_obj);
+    compile_c(&cc, "ffi/interface.c", &interface_obj);
     compile_c(&cc, "ffi/netlink.c", &netlink_obj);
     compile_c(&cc, "ffi/networkd.c", &networkd_obj);
 
-    let mut objects = vec![native_obj, netlink_obj, networkd_obj];
+    let mut objects = vec![native_obj, interface_obj, netlink_obj, networkd_obj];
     let fortran_enabled = env::var_os("CARGO_FEATURE_FORTRAN_ROUTING").is_some();
     if fortran_enabled {
         let fc = env::var_os("FC").unwrap_or_else(|| OsString::from("gfortran"));
