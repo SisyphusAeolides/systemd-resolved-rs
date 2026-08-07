e429e85ce571b6426b965cd5044038b9e950e10c
Kenny Glowner <SisyphusAeolides@pm.me>
2026-08-07 08:16:07 -0500

    Activate DNSSEC canonicalization

--- src/dnssec.rs
+++ src/dnssec.rs
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+pub mod canonical;
+
 use crate::wire::{dnskey_key_tag, parse_dnskey, parse_ds, ResourceRecord, WireError};
 use std::error::Error;
 use std::fmt;
